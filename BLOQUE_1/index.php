ec0a44fd1c1542713fcade5d2907904f9928753e
sehemu92 <sehemu@alumnatflorida.es>
2025-09-20 12:55:15 +0200

    prueba

---
+++ BLOQUE_1/index.php
@@ -0,0 +1,10 @@
+<?php
+    echo "Hola Mundo <br>";
+    echo "<br>";
+    echo "paco hola";
+    echo "<br>";
+    echo "<br>";
+    echo $_SERVER['HTTP_HOST'];
+    echo "<br>";
+    echo "<br>";
+    echo "paco";f
