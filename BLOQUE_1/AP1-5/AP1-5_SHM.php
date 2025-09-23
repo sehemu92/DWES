7054ea7d6cbcaa2b3cffc34ecadc7c736a0cdf10
sehemu92 <sehemu@alumnatflorida.es>
2025-09-23 19:50:56 +0200

    Revisado en detalle todos los puntos, pero no funciona la consulta DELETE

--- BLOQUE_1/AP1-5/AP1-5_SHM.php
+++ BLOQUE_1/AP1-5/AP1-5_SHM.php
@@ -63,31 +63,51 @@ while($row = $result->fetch_assoc()){ //fetch_assoc() ----> Se utiliza para reco
 
 //AÑADIR REGISTRO A UNA TABLA
 //Datos a insertar
-$nombre="Margarita";
-$estado =4;
+/*OPCION 1
+ * Introducir registro mediante variables
+ * $nombre="Margarita";
+ * $estado =4;
+ *
+ */
 
 //Creamos la consulta
-$sql="INSERT INTO usuarios (nombre, estado) VALUES ('$nombre', '$estado')";
+//$sql="INSERT INTO usuarios (nombre, estado) VALUES ('$nombre', '$estado')";//Inserción mediante variables
+$sql="INSERT INTO usuarios (nombre, estado) VALUES ('WINDOWS', false)";
 
 //Ejecutamos la consulta
-$result =$conexion->query($sql);
+/**ORIGINARIAMENTE LO TENIA ASÍ, FUNCIONABA AUNQUE AL INCLUIR DOS VALORES IGUALES YA DABA ERROR
+* $conexion->query($sql);
+ *
+* //Comprobamos que se ha realizado
+* /*if ($result){
+* //Para extraer la ultima ID incluida:
+* $nueva_id =$conexion->insert_id; //Esta es la ide del insert con "insert_id"
+* echo "Registro insertado con éxito. El nuevo registro tiene la id: " .$nueva_id;
+* echo "<br>";
+* }else{
+* echo "error al insertar: " .$conexion->error;
+* }*/
 
-//Comprobamos que se ha realizado
-if ($result){
+/*Hacer un try/catch de la función "$conexion->query($sql);"*/
+try{
+    $conexion->query($sql);
     //Para extraer la ultima ID incluida:
-    $nueva_id =$conexion->insert_id; //Esta es la ide del insert con "insert_id"
-    echo "Registro insertado con éxito. El nuevo registro tiene la id: " .$nueva_id;
+    $id=$conexion->insert_id;//"insert_id" te permite acceder al ÚLTIMO ID creado
+    echo "Registro insertado con éxito. El nuevo registro tiene la id: " .$id;
     echo "<br>";
-}else{
-    echo "error al insertar: " .$conexion->error;
+}catch (mysqli_sql_exception $e){//Se utiliza este tipo "mysqli_sql_exception" de error porque es el unico error que puede dar
+    die("Se ha producido el siguiente error: "."<br>" . $e->getMessage()) ."en la linea: ".$e->getLine();
 }
+
 
 //MODIFICAR ESTADO Y ACTUALIZAR REGISTRO
 //Realizamos la actualización
-$sql = "UPDATE usuarios SET estado=true WHERE id=".$nueva_id;
+$sql = "UPDATE usuarios SET estado=true WHERE id=".$id;
+$sql2 = "UPDATE usuarios SET nombre='APPLE' WHERE id=".$id;
 try {
     $conexion->query($sql);
-    echo "Se ha realizado correctamente la actualziación de la id:" . $nueva_id . "<br>";
+    $conexion->query($sql2);
+    echo "Se ha realizado correctamente la actualizacion de la id:" . $id . "<br>";
     echo "<br>";
 }catch (mysqli_sql_exception $e){
     die ("Se ha producido el siguiente error:<br>".$e->getMessage().". En la línea:".$e->getLine()."<br>");
@@ -112,10 +132,10 @@ if($result->num_rows >0){ //Esta condición le está indicando si hay más de un
 
 //BORRAR ÚLTIMO INSERTADO Y ACTUALIZAR
 //Realizamos el borrado de un dato
-$sql = "DELETE FROM usuarios WHERE id=".$nueva_id;
+$sql = "DELETE FROM usuarios WHERE id=".$id;
 try {
-    $conn->query($sql);
-    echo "Se ha realizado correctamente el borrado de la id:" . $nueva_id . "<br>";
+    $conexion->query($sql);
+    echo "Se ha realizado correctamente el borrado de la id:" . $id . "<br>";
 }catch (mysqli_sql_exception $e){
     die ("Se ha producido el siguiente error:<br>".$e->getMessage().". En la línea:".$e->getLine()."<br>");
 }
