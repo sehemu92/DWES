65ceccb539477139d03bcbae6fa6689ae0c24964
sehemu92 <sehemu@alumnatflorida.es>
2025-09-30 22:52:28 +0200

    AP3-1 View solucion propia, NO FUNCIONA

--- BLOQUE_1/AP3-1/src/views/View.php
+++ BLOQUE_1/AP3-1/src/views/View.php
@@ -1,1 +1,42 @@
 <?php
+if (!isset($datos)) {
+    $datos = [];
+}
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>AP3-1 - VISTA</title>
+</head>
+<body>
+<h1>AP3-1</h1>
+<ul>
+    <?php foreach ($datos as $key => $value): ?>
+        <li>
+            <?php
+                echo "title: " .$datos[0];
+            ?>
+        </li>
+        <li>
+            <?php
+            echo "keyworks: " .$datos[1];
+            ?>
+        </li>
+        <li>
+            <?php
+            echo "description: " .$datos[2];
+            ?>
+        </li>
+        <li>
+            <?php
+            echo "content: " .$datos[3];
+            ?>
+        </li>
+    <?php endforeach; ?>
+</ul>
+</body>
+</html>
+
+
+
